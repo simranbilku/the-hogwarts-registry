0f15a8c2f690cdfbfe8d23a3ad64aea33dca81ac
Simran Bilku <sbilku@hotmail.co.uk>
2026-06-04 12:02:46 +0100

    Added User Repository

--- registry/src/main/java/com/hogwarts/registry/repos/UserRepository.java
+++ registry/src/main/java/com/hogwarts/registry/repos/UserRepository.java
@@ -1,4 +1,9 @@
 package com.hogwarts.registry.repos;
 
-public class UserRepository {
+import com.hogwarts.registry.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
