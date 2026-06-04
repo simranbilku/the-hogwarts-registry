628bfbd89458067aa054da2f249efff7d3245cc9
Simran Bilku <sbilku@hotmail.co.uk>
2026-06-04 15:01:47 +0100

    Update User entity with House enum and getters and setters

--- registry/src/main/java/com/hogwarts/registry/models/User.java
+++ registry/src/main/java/com/hogwarts/registry/models/User.java
@@ -15,4 +15,54 @@ public class User {
     @Enumerated(EnumType.STRING)
     private House house;
 
+    public User() {
+
+    }
+
+    public User(String username, String email, String password, House house) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.house = house;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public House getHouse() {
+        return house;
+    }
+
+    public void setHouse(House house) {
+        this.house = house;
+    }
 }
