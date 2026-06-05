697d0c2d5a0213b89cd3b1ad252fa35731f75255
Simran Bilku <sbilku@hotmail.co.uk>
2026-06-05 10:17:23 +0100

    Add User service and DTOs

--- registry/src/main/java/com/hogwarts/registry/DTOs/CreateUserRequest.java
+++ registry/src/main/java/com/hogwarts/registry/DTOs/CreateUserRequest.java
@@ -1,4 +1,51 @@
-package com.hogwarts.registry.DTOs;
+package com.hogwarts.registry.dtos;
+
+import com.hogwarts.registry.models.House;
 
 public class CreateUserRequest {
+
+    private String username;
+    private String email;
+    private String password;
+    private House house;
+
+
+    public CreateUserRequest() {
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public House getHouse() {
+        return house;
+    }
+
+    public void setHouse(House house) {
+        this.house = house;
+    }
 }
--- registry/src/main/java/com/hogwarts/registry/Services/UserService.java
+++ registry/src/main/java/com/hogwarts/registry/Services/UserService.java
@@ -1,4 +1,53 @@
-package com.hogwarts.registry.Services;
+package com.hogwarts.registry.services;
 
+import com.hogwarts.registry.dtos.CreateUserRequest;
+import com.hogwarts.registry.dtos.UserResponse;
+import com.hogwarts.registry.models.User;
+import com.hogwarts.registry.repos.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    // get all users
+    public List<UserResponse> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(this::mapToResponse)
+                .toList();
+    }
+
+    // create user
+    public UserResponse addUser(CreateUserRequest newUserRequest) {
+
+        if (userRepository.existsByEmail(newUserRequest.getEmail())) {
+            throw new IllegalArgumentException("This user email is already in use!");
+        }
+
+        User user = new User();
+        user.setUsername(newUserRequest.getUsername());
+        user.setEmail(newUserRequest.getEmail());
+        user.setPassword(newUserRequest.getPassword());
+        user.setHouse(newUserRequest.getHouse());
+
+        User savedUser = userRepository.save(user);
+
+        return mapToResponse(savedUser);
+    }
+
+    private UserResponse mapToResponse(User user) {
+        return new UserResponse(
+                user.getUserId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getHouse()
+        );
+    }
 }
