8fdadd448b7feb8b7c712f901c7e9be9f491973f
Simran Bilku <sbilku@hotmail.co.uk>
2026-06-08 10:34:42 +0100

    Add CORS configuration for frontend

--- registry/src/main/java/com/hogwarts/registry/Controllers/UserController.java
+++ registry/src/main/java/com/hogwarts/registry/Controllers/UserController.java
@@ -7,9 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:5500"})
 @RestController
 @RequestMapping(value = "/api/users")
 public class UserController {
